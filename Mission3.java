3ae00a1338b5bee87f64b601a7d18a35ae5c6b94
fabbitox <64521259+fabbitox@users.noreply.github.com>
2023-04-06 17:46:56 +0900

    change the number of leap years and print format

--- Mission3.java
+++ Mission3.java
@@ -3,11 +3,11 @@ package chapter5;
 public class Mission3 {
 
 	public static void main(String[] args) {
-		int[] leaps = new int[10];
+		int[] leaps = new int[40];
 		int count = 0;
 		int year = 2024;
 		
-		while (count < 10) {
+		while (count < 40) {
 			if (year % 100 != 0) {
 				leaps[count++] = year;
 			} else if (year % 400 == 0) {
@@ -16,8 +16,11 @@ public class Mission3 {
 			year += 4;
 		}
 		
-		for (int y: leaps) {
-			System.out.print(y + "\t");
+		for (int i = 0; i < 40; i++) {
+			System.out.print(leaps[i] + "\t");
+			if (i % 5 == 4) {
+				System.out.println();
+			}
 		}
 		System.out.println();
 	}
