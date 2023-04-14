13b90b815e3fac9c2551f33d298f070ae88e3010
fabbitox <64521259+fabbitox@users.noreply.github.com>
2023-04-14 09:27:01 +0900

    correct file

--- Mission9.java
+++ Mission9.java
@@ -6,29 +6,39 @@ public class Mission9 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int weight = sc.nextInt();
-		
-		while (weight < 10 || weight > 100) {
-			System.out.println("10 이상 100 이하의 수를 입력하세요");
-			weight = sc.nextInt();
-		}
-		// 1개 이상씩 사용되어야 하기 때문에 1개씩 씀
-		int two = 1;
-		int three = 1;
-		int five = 1;
-		for (; two <= 10; two++) {
-			for (three = 1; three <= 10; three++) {
-				for (five = 1; five <= 10; five++) {
-					int val = two * 2 + three * 3 + five * 5;
-					if (val > weight) {
-						break;
-					}
-					if (val == weight) {
-						System.out.println("(" + two + ", " + three + ", " + five + ")");
-					}
+
+		int coreW = weight - 10;
+		int remW;
+		int two;
+		int three;
+
+		for (int i = 0; i < 10; i++) {
+			remW = coreW - i * 5;
+			int divide2 = remW / 2;
+			switch (remW % 2) {
+			case 0:
+				two = divide2;
+				three = 0;
+				while (two >= 0 && three <= 9) {
+					if (two <= 9)
+						System.out.println("(" + (two + 1) + ", " + (three + 1) + ", " + (i + 1) + ")");
+					two -= 3;
+					three += 2;
 				}
+				break;
+			case 1:
+				two = divide2 - 1;
+				three = 1;
+				while (two >= 0 && three <= 9) {
+					if (two <= 9)
+						System.out.println("(" + (two + 1) + ", " + (three + 1) + ", " + (i + 1) + ")");
+					two -= 3;
+					three += 2;
+				}
+				break;
 			}
 		}
-		
+
 		sc.close();
 	}
 }
